8f8da84352ee036da90303a5f1c5feddbd848ead
amillena <amillena@yahoo.com>
2017-04-05 12:33:35 -0700

    second upload

---
+++ keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+	consumer_key:         'ZqqUy6acZWuTTrFB48LZIqHoN',
+  	consumer_secret:      'ncQuUhsQ4KRlyt19pIIVhd4UQzYTO7r0TvFiKF0GlKGWthK5Su',
+  	access_token:         '1214758495-JGmPiZgfgiFbQ4puHQHeaeIU1JgdBWPLL3cUPTw',
+  	access_token_secret:  'ZCQmkqZPKgVUOTF2cQUELzu7lz2JqlJQbCxJNGssf8Fka',
+};
